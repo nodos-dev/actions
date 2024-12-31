718d768976d6cacdc541626651512a55e2ddeef9
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2024-12-31 12:08:53 +0300

    Prepare build strategy with script
    Co-authored-by: Furkan <furkancan.turan@nodos.dev>

--- scripts/prep_runner_list.py
+++ scripts/prep_runner_list.py
@@ -11,8 +11,8 @@ import sys
 
 event_name = None
 ref_name = None
-workflow_dispatch_input_linux = None
-workflow_dispatch_input_windows = None
+workflow_input_linux = None
+workflow_input_windows = None
 sign = None
 push_event_defaults = None
 
@@ -21,10 +21,10 @@ for i in range(1, len(sys.argv), 2):
         event_name = sys.argv[i + 1]
     elif sys.argv[i] == "--ref-name":
         ref_name = sys.argv[i + 1]
-    elif sys.argv[i] == "--workflow-dispatch-input-linux":
-        workflow_dispatch_input_linux = sys.argv[i + 1]
-    elif sys.argv[i] == "--workflow-dispatch-input-windows":
-        workflow_dispatch_input_windows = sys.argv[i + 1]
+    elif sys.argv[i] == "--workflow-input-linux":
+        workflow_input_linux = sys.argv[i + 1]
+    elif sys.argv[i] == "--workflow-input-windows":
+        workflow_input_windows = sys.argv[i + 1]
     elif sys.argv[i] == "--sign":
         sign = sys.argv[i + 1]
     elif sys.argv[i] == "--push-event-defaults":
@@ -34,10 +34,10 @@ default_tags = ['self-hosted', ref_name]
 
 runner_list = [] # E.g. [["self-hosted", "Linux", "dev"], ["self-hosted", "Windows", "dev"]]
 
-if event_name == "workflow_dispatch":
-    if workflow_dispatch_input_linux == "true":
+if event_name == "workflow_dispatch" or event_name == "workflow_call":
+    if workflow_input_linux == "true":
         runner_list.append(default_tags + ["Linux"])
-    if workflow_dispatch_input_windows == "true":
+    if workflow_input_windows == "true":
         win_labels = default_tags + ["Windows"]
         if sign == "true":
             win_labels.append("signer")
