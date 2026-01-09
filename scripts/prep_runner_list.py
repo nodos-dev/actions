3e44fc6e885fc526aec88b7bcfce6b4621f50dac
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2026-01-09 18:53:00 +0300

    Use architecture runner labels

--- scripts/prep_runner_list.py
+++ scripts/prep_runner_list.py
@@ -17,10 +17,11 @@ def main():
     # Define the arguments (supporting both flags and 'true'/'false' string)
     parser.add_argument("--event-name", required=True, help="GitHub event name (e.g., push, workflow_dispatch)")
     parser.add_argument("--ref-name", required=True, help="GitHub ref name (e.g., branch or tag name)")
-    parser.add_argument("--workflow-input-linux", type=str_to_bool, nargs="?", default=False, help="Enable Linux workflow input (true/false or flag)")
-    parser.add_argument("--workflow-input-windows", type=str_to_bool, nargs="?", default=False, help="Enable Windows workflow input (true/false or flag)")
+    parser.add_argument("--workflow-input-linux", type=str_to_bool, nargs="?", default=False, help="Enable Linux builds (true/false or flag)")
+    parser.add_argument("--workflow-input-windows", type=str_to_bool, nargs="?", default=False, help="Enable Windows builds (true/false or flag)")
     parser.add_argument("--sign", type=str_to_bool, nargs="?", default=False, help="Enable signing (Windows only, true/false or flag)")
     parser.add_argument("--push-event-defaults", type=str, help="JSON string with push event defaults")
+    parser.add_argument("--arch", type=str, help="Target build architecture", default="X64")
     # Parse the arguments
     args = parser.parse_args()
     
@@ -29,6 +30,7 @@ def main():
     ref_name = args.ref_name
     workflow_input_linux = args.workflow_input_linux
     workflow_input_windows = args.workflow_input_windows
+    architecture = args.arch
     sign = args.sign
     push_event_defaults = args.push_event_defaults
 
@@ -37,9 +39,9 @@ def main():
 
     if event_name in ["workflow_dispatch", "workflow_call"]:
         if workflow_input_linux:
-            runner_list.append(["Linux"])
+            runner_list.append(["Linux", architecture])
         if workflow_input_windows:
-            win_labels = ["Windows"]
+            win_labels = ["Windows", architecture]
             if sign:
                 win_labels.append("signer")
             runner_list.append(win_labels)
@@ -47,9 +49,9 @@ def main():
         if push_event_defaults:
             push_event_defaults = json.loads(push_event_defaults)
             if push_event_defaults.get("linux", False):
-                runner_list.append(["Linux"])
+                runner_list.append(["Linux", architecture])
             if push_event_defaults.get("windows", False):
-                win_labels = ["Windows"]
+                win_labels = ["Windows", architecture]
                 if push_event_defaults.get("sign", False):
                     win_labels.append("signer")
                 runner_list.append(win_labels)
